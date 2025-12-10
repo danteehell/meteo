41a98a94773a0abf4b0d3c340857d4e5cf211113
Darya15-4 <yasdarya7@gmail.com>
2025-12-10 19:15:43 +0300

    pagination

--- django-project/meteo/settings.py
+++ django-project/meteo/settings.py
@@ -117,3 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#критерий3 3
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
+}
+#критерий3 3
