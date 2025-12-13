ccef2a49f8eaf75f332ef2ea2e9b16e91aa6f696
Darya15-4 <yasdarya7@gmail.com>
2025-12-13 13:13:59 +0300

    @action(methods=['GET'], detail=False)

--- django-project/meteo/filters.py
+++ django-project/meteo/filters.py
@@ -4,6 +4,7 @@ from .models import City
 class CityFilter(django_filters.FilterSet):
     country = django_filters.CharFilter(field_name='country', lookup_expr='iexact')
     min_lat = django_filters.NumberFilter(field_name='latitude', lookup_expr='gte')
+    
 
     class Meta:
         model = City
--- django-project/meteo/views.py
+++ django-project/meteo/views.py
@@ -7,6 +7,9 @@ from .models import City, WeatherIcon
 from .serializers import CitySerializer, WeatherIconSerializer
 from django.db.models import Q
 from .filters import CityFilter
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
 
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
@@ -17,7 +20,14 @@ class CityViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return City.objects.filter((Q(country='Россия') | Q(country='Польша')) & Q(latitude__lte=50) & ~Q(longitude__lte=30)
         )
-        #критерий3 4
+    #критерий3 4
+
+
+    @action(methods=['GET'], detail=False)
+    def count(self, request):
+        return Response({'count': City.objects.count()})
+    
+
 class WeatherIconViewSet(viewsets.ModelViewSet):
     queryset = WeatherIcon.objects.all()
     serializer_class = WeatherIconSerializer
@@ -27,4 +37,13 @@ class WeatherIconViewSet(viewsets.ModelViewSet):
     (Q(name__icontains="sun") | Q(name__icontains="cloud")) & ~Q(image__isnull=True) & Q(image_url="")
     #критерий4 1
 )
+    
+
+    @action(methods=['GET'], detail=False)
+    def with_file(self, request):
+        count = WeatherIcon.objects.filter(Q(image__isnull = False) | Q(image_url__isnull = False)).count()
+        return Response({'count': count})
+    
+
+    
 #критерий3 1
