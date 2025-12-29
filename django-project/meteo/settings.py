f411df6ddd18fac1b7b3dd2e86221918eee13f79
Darya15-4 <yasdarya7@gmail.com>
2025-12-29 22:08:32 +0300

    attempt-2

--- django-project/meteo/settings.py
+++ django-project/meteo/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback_key')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
